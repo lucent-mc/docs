480f9fcb173828e2769c6a02f6ef7f98c70cbdd3
Mia <mia@mia.cx>
2025-10-06 14:18:40 +0200

    Obsidian on : 2025-10-06 14:18:40
    Affected files:
    quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -60,9 +60,17 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
+    Component.Graph({
+      localGraph: {
+        depth: 3,
+      }
+    }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
+    Component.RecentNotes({
+      title: "Recently updated",
+      limit: 5,
+    }),
   ],
 }
 
