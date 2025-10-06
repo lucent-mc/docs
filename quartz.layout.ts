d5dc1fa010c1283dd05ad84dc611af8f39cd2009
Mia <mia@mia.cx>
2025-10-06 15:09:53 +0200

    re-add reading mode to folder page

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -87,6 +87,7 @@ export const defaultListPageLayout: PageLayout = {
           grow: true,
         },
         { Component: Component.Darkmode() },
+        { Component: Component.ReaderMode() },
       ],
     }),
     Component.Explorer(),
