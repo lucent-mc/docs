df5b849646d65760c75d377e0efc4bf06e9270ed
Mia <mia@mia.cx>
2025-10-08 22:44:47 +0200

    custom sort func on all pages

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -57,7 +57,26 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      sortFn: (a, b) => {
+        if ((!a.isFolder && !b.isFolder) || (a.isFolder && b.isFolder)) {
+          return (a.sortName ?? a.displayName).localeCompare(
+            b.sortName ?? b.displayName,
+            undefined,
+            {
+              numeric: true,
+              sensitivity: "base",
+            },
+          )
+        }
+
+        if (!a.isFolder && b.isFolder) {
+          return 1
+        } else {
+          return -1
+        }
+      },
+    }),
   ],
   right: [
     Component.Graph({
@@ -99,10 +118,14 @@ export const defaultListPageLayout: PageLayout = {
     Component.Explorer({
       sortFn: (a, b) => {
         if ((!a.isFolder && !b.isFolder) || (a.isFolder && b.isFolder)) {
-          return (a.sortName ?? a.displayName).localeCompare((b.sortName ?? b.displayName), undefined, {
-            numeric: true,
-            sensitivity: "base",
-          })
+          return (a.sortName ?? a.displayName).localeCompare(
+            b.sortName ?? b.displayName,
+            undefined,
+            {
+              numeric: true,
+              sensitivity: "base",
+            },
+          )
         }
 
         if (!a.isFolder && b.isFolder) {
